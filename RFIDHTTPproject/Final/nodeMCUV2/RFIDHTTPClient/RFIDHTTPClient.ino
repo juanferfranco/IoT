74849bdff3064eabbbb52ee2c65965852da967d3
Juan Fernando Franco Higuita <juanferfranco@gmail.com>
2017-05-31 14:40:51 -0500

    WiFiManager support
    A feature to handle WiFi credentials by means of softAP, captive portal
    and web server.

--- RFIDHTTPproject/Final/nodeMCUV2/RFIDHTTPClient/RFIDHTTPClient.ino
+++ RFIDHTTPproject/Final/nodeMCUV2/RFIDHTTPClient/RFIDHTTPClient.ino
@@ -1,13 +1,13 @@
-/**
-
-*/
-
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include <MFRC522.h>
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 #define USE_SERIAL Serial
 ESP8266WiFiMulti WiFiMulti;
@@ -19,10 +19,20 @@ MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 #define LEDCARD 15
 
+
+#define INITPAYLOAD 38  // WARNING!!!!!!!! Adjust this constant to point at the end of strReq (just after '/')
 char strReq[64] = "http://ensayoerror.pythonanywhere.com/";
-#define INITPAYLOAD 38
+
 
 void setup() {
+  ////////////////////////////////////
+  // Temporal code to test WiFiManager.
+  // Use this code to clear the wifi credentials.
+
+  WiFi.disconnect();
+  delay(1000);
+  ///////////////////////////////////
+
   pinMode(LEDCARD, OUTPUT);
   SPI.begin();      // Init SPI bus
   mfrc522.PCD_Init();   // Init MFRC522
@@ -35,7 +45,11 @@ void setup() {
     USE_SERIAL.flush();
     delay(1000);
   }
-  WiFiMulti.addAP("UPBWiFi");
+  WiFiManager wifiManager;
+  //WiFiMulti.addAP("UPBWiFi");
+  wifiManager.autoConnect("RFIDTIC");
+
+  USE_SERIAL.println("TEST");
 }
 
 void loop() {
@@ -48,35 +62,34 @@ void loop() {
     return;
   }
 
-  digitalWrite(LEDCARD, HIGH);
-  Serial.println();
-  Serial.println("Sending UID");
-
   strReq[INITPAYLOAD] = NULL;
 
   for (uint8_t i = 0; i < mfrc522.uid.size; i++) {
     sprintf(strTmp, "%02X", mfrc522.uid.uidByte[i]);
     strcat(strReq, strTmp);
   }
-
   // Halt PICC
   mfrc522.PICC_HaltA();
   // Stop encryption on PCD
   mfrc522.PCD_StopCrypto1();
 
+  USE_SERIAL.println("TEST 2");
+
+
   // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED)) {
     HTTPClient http;
-    USE_SERIAL.print("[HTTP] begin...\n");
-    http.begin(strReq); //HTTP
+
+    USE_SERIAL.println("Sending UID");
+    http.begin(strReq);
     USE_SERIAL.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
     int httpCode = http.GET();
     // httpCode will be negative on error
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
       USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
+      
+      
       // file found at server
       if (httpCode == HTTP_CODE_OK) {
         String payload = http.getString();
@@ -86,7 +99,12 @@ void loop() {
       USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
     http.end();
+    if(httpCode == HTTP_CODE_OK){
+      digitalWrite(LEDCARD, HIGH);
+      delay(500);
+      digitalWrite(LEDCARD, LOW);
+      delay(500);
+    }
   }
-  digitalWrite(LEDCARD, LOW);
 }
 
