e85849cf4b5c9c88e6182f6b83162723d18ff66e
Juan Fernando Franco Higuita <juanf.franco@upb.edu.co>
2017-10-18 05:44:43 -0500

    Remove some comments

--- RFIDHTTPproject/Prototyping/RFIDTests/NodeMCUV2/DumpInfoNodeMCURFID/DumpInfoNodeMCURFID.ino
+++ RFIDHTTPproject/Prototyping/RFIDTests/NodeMCUV2/DumpInfoNodeMCURFID/DumpInfoNodeMCURFID.ino
@@ -5,40 +5,6 @@
 #define LED_RFID D8 // LED RFID read 
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
-
-/*
-void setup() {
-	Serial.begin(9600);		// Initialize serial communications with the PC
-	SPI.begin();			// Init SPI bus
-	mfrc522.PCD_Init();		// Init MFRC522
-}
-
-void loop() { 
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    delay(50);
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    delay(50);
-    return;
-  }
-  // Show some details of the PICC (that is: the tag/card)
-  Serial.print(F("Card UID:"));
-  dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-  Serial.println();
-}
-
-// Helper routine to dump a byte array as hex values to Serial
-void dump_byte_array(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
-}
-*/
-
 void setup() {
   Serial.begin(115200); // Initialize serial communications with the PC
   SPI.begin();      // Init SPI bus
